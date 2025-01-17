c854b1c318dcb2db7721a8015bd968e991e3ceea
thallencar <alencar.thalita@gmail.com>
2025-01-16 23:20:23 -0300

    feat: add support for multiple exceptions and custom error handling

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
@@ -15,7 +16,13 @@ namespace CashFlow.Application.UseCases.Expenses.Register
         {
             var validator = new RegisterExpenseValidator();
 
-            var reuslt = validator.Validate(request);
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
         }
     }
 }
