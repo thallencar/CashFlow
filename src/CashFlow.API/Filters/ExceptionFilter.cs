018c1b3e6a9c7b6b23c662fdf073dda78793af78
thallencar <alencar.thalita@gmail.com>
2025-01-18 01:48:10 -0300

    feat: add resource error messages file

--- src/CashFlow.API/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -43,7 +44,7 @@ namespace CashFlow.API.Filters
 
         private void ThrowUnknownError(ExceptionContext context)
         { 
-            var errorResponse = new ResponseErrorJson("Unknown error.");
+            var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
